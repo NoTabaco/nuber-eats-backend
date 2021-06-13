465c5f81a45c8a6870cbb5f1f306373a40ebfac2
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-06-14 02:00:54 +0900

    Updated Create Account Mutation

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,16 +1,27 @@
+import { Field, ObjectType, registerEnumType } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Column, Entity } from 'typeorm';
 
-type UserRole = 'client' | 'owner' | 'delivery';
+enum UserRole {
+  Client,
+  Owner,
+  Delivery,
+}
 
+registerEnumType(UserRole, { name: 'UserRole' });
+
+@ObjectType()
 @Entity()
 export class User extends CoreEntity {
+  @Field(type => String)
   @Column()
   email: string;
 
+  @Field(type => String)
   @Column()
   password: string;
 
-  @Column()
+  @Field(type => UserRole)
+  @Column({ type: 'enum', enum: UserRole })
   role: UserRole;
 }
