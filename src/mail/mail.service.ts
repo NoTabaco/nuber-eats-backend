2a070a85271814bdb4c8c9bd080205a4b7808c1a
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-06-24 19:46:51 +0900

    Refactoring

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,33 +2,31 @@ import * as FormData from 'form-data';
 import got from 'got';
 import { Inject, Injectable } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
-import { MailModuleOptions } from './mail.interfaces';
+import { EmailVars, MailModuleOptions } from './mail.interfaces';
 
 @Injectable()
 export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS)
     private readonly options: MailModuleOptions,
-  ) {
-    this.sendEmail('Kimchi', `template_email`);
-  }
+  ) {}
 
-  private async sendEmail(subject: string, template: string) {
+  private async sendEmail(
+    subject: string,
+    template: string,
+    { key, value }: EmailVars,
+  ) {
     const form = new FormData();
-    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+    form.append(
+      'from',
+      `Kimchi-King from Nuber Eats <mailgun@${this.options.domain}>`,
+    );
     form.append('to', `adslonese@gmail.com`);
     form.append('subject', subject);
     form.append('template', template);
-    form.append(
-      'h:X-Mailgun-Variables',
-      `{
-        "username": "Kimchi-King",
-        "code": "Perfect-Kimchi"
-        }`,
-    );
-    const response = await got(
-      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
-      {
+    form.append(key, value);
+    try {
+      await got(`https://api.mailgun.net/v3/${this.options.domain}/messages`, {
         method: 'POST',
         headers: {
           Authorization: `Basic ${Buffer.from(
@@ -36,7 +34,16 @@ export class MailService {
           ).toString('base64')}`,
         },
         body: form,
-      },
-    );
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  sendVerificationEmail(email: string, code: string) {
+    this.sendEmail('Verify Your Email', 'template_email', {
+      key: 'h:X-Mailgun-Variables',
+      value: JSON.stringify({ username: email, code }),
+    });
   }
 }
