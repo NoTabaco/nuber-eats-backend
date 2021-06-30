d0c12798a9d5570bd7f118c73fa12ff3d74fb316
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-06-30 21:50:56 +0900

    Finished login E2E Testing

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -12,8 +12,14 @@ jest.mock('got', () => {
 
 const GRAPHQL_ENDPOINT = '/graphql';
 
+const testUser = {
+  email: 'shin-kimchi@perfect.com',
+  password: '12345',
+};
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  let jwtToken: string;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -29,7 +35,6 @@ describe('UserModule (e2e)', () => {
   });
 
   describe('createAccount', () => {
-    const EMAIL = 'shin-kimchi@perfect.com';
     it('should create account', () => {
       return request(app.getHttpServer())
         .post(GRAPHQL_ENDPOINT)
@@ -38,8 +43,8 @@ describe('UserModule (e2e)', () => {
           mutation {
             createAccount(
               input: { 
-                email: "${EMAIL}", 
-                password: "12345", 
+                email: "${testUser.email}", 
+                password: "${testUser.password}", 
                 role: Owner 
               }) {
               ok
@@ -60,29 +65,95 @@ describe('UserModule (e2e)', () => {
         .post(GRAPHQL_ENDPOINT)
         .send({
           query: `
-          mutation {
-            createAccount(
-              input: { 
-                email: "${EMAIL}", 
-                password: "12345", 
-                role: Owner 
-              }) {
-              ok
-              error
-            }
-          }          
-          `,
+            mutation {
+              createAccount(
+                input: { 
+                  email: "${testUser.email}", 
+                  password: "${testUser.password}", 
+                  role: Owner 
+                }) {
+                ok
+                error
+              }
+            }          
+            `,
         })
         .expect(200)
         .expect(res => {
           expect(res.body.data.createAccount.ok).toBe(false);
-          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+          expect(res.body.data.createAccount.error).toBe(
+            'There is a user with that email already',
+          );
+        });
+    });
+  });
+
+  describe('login', () => {
+    it('should login with correct credentials', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+            mutation {
+              login(
+                input: { 
+                  email: "${testUser.email}", 
+                  password: "${testUser.password}" 
+                }) {
+                ok
+                error
+                token
+              }
+            }
+            `,
+        })
+        .expect(200)
+        .expect(res => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(true);
+          expect(login.error).toBe(null);
+          expect(login.token).toEqual(expect.any(String));
+          jwtToken = login.token;
+        });
+    });
+
+    it('should not be able to login with wrong credentials', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+            mutation {
+              login(
+                input: { 
+                  email: "${testUser.email}", 
+                  password: "kimchi" 
+                }) {
+                ok
+                error
+                token
+              }
+            }
+            `,
+        })
+        .expect(200)
+        .expect(res => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(false);
+          expect(login.error).toBe('Wrong password');
+          expect(login.token).toBe(null);
         });
     });
   });
 
   it.todo('userProfile');
-  it.todo('login');
   it.todo('me');
   it.todo('verifyEmail');
   it.todo('editProfile');
