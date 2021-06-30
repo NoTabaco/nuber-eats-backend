f602a977ed7454b8378633845848e21e05342a88
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-06-30 20:09:35 +0900

    Finished createAccount E2E Testing

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,14 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
@@ -20,7 +28,59 @@ describe('UserModule (e2e)', () => {
     app.close();
   });
 
-  it.todo('createAccount');
+  describe('createAccount', () => {
+    const EMAIL = 'shin-kimchi@perfect.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(
+              input: { 
+                email: "${EMAIL}", 
+                password: "12345", 
+                role: Owner 
+              }) {
+              ok
+              error
+            }
+          }          
+          `,
+        })
+        .expect(200)
+        .expect(res => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(
+              input: { 
+                email: "${EMAIL}", 
+                password: "12345", 
+                role: Owner 
+              }) {
+              ok
+              error
+            }
+          }          
+          `,
+        })
+        .expect(200)
+        .expect(res => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+        });
+    });
+  });
+
   it.todo('userProfile');
   it.todo('login');
   it.todo('me');
